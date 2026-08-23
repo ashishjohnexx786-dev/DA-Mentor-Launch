b121628e76d8e407eab6cc14b598403981af2c73
Ashish Kumar <ashishjohnexx786@gmail.com>
2026-08-23 22:08:06 +0530

    Refresh offline cache for top-bar settings

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='da-mentor-os-v4.2.1';
+const CACHE='da-mentor-os-v4.3';
 const ASSETS=['./','./index.html','./styles.css','./curriculum.js','./app.js','./enhancements.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
